52e4e9101f443749b7fe00086b193843e283a99f
RICHA CHAUDHARY <richa.chaudhary197@gmail.com>
2025-08-19 20:19:40 +0530

    jobBoardLite

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 let navbar = document.querySelector('.header .container .navbar');
 
 document.querySelector('#menu-button').onclick = () =>{
-    navbar.classList.toggle('active');
-}
+   navbar.classList.toggle('active'); }
 
-window.onscroll = () => {
-    navbar.classList.remove('active');
+    window.onscroll = () => {
+     navbar.classList.remove('active');
 }
+// let menuBtn = document.querySelector("#menu-button");
+//     let navbar = document.querySelector(".header .container .navbar");
+
+//     menuBtn.onclick = () => {
+//         navbar.classList.toggle("active");
+//     };
